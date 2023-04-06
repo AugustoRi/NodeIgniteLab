01d09f803600fdf9bfe4e1f17e5114a382e65b54
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-06 13:40:07 -0300

    feat: notification post route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
+  list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Voce tem uma nova notificacao',
+        category: 'social',
+        recipientId: randomUUID(),
+        createdAt: new Date(),
+        readAt: null,
+      },
+    });
   }
 }
