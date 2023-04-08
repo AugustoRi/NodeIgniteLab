7ddce53d6f0b66bee5dfd1c921db43bb6370cab4
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-08 11:04:02 -0300

    feat: add repository prisma

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
+import type { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationsMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,18 @@ export class PrismaNotificationsMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        content: new Content(raw.content),
+        category: raw.category,
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
